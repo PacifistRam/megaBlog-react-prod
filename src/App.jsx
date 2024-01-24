a892b9840fe8e33c174909f326cd91bae547de4b
PacifistRam <141394720+PacifistRam@users.noreply.github.com>
2024-01-24 13:23:08 +0530

    added dependencies and appwrite intialization

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import conf from '../conf/conf'
+function App() {
+
+  //console.log(conf.appWriteCollectionId)
+  return (
+    <>
+     <h1>Hello world</h1>
+    
+    </>
+  )
+}
+
+export default App
